2552c9ec44674fb3e167b94e50f57dbfe31c4a32
shuaib <yyshu18@gmail.com>
2017-12-02 23:18:04 +0800

    update route

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 var user = require('./controllers/user')
 
-router.route('/register') 
+router.route('/register') //endpoint(resources)
 	.post(user.post)
 
 module.exports = router
