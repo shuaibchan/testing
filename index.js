1f2342915e3f898e641c872ccf32377f6263b8db
shuaib <yyshu18@gmail.com>
2017-12-02 23:19:23 +0800

    add body parse

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var express = require('express')
-var app = express()
-var router = require("./src/router")
-var bodyParser = require('body-parser')
+var express = require('express')//import  supayaexpress library
+var app = express()//initialize express
+var router = require("./src/router")//import router
+var bodyParser = require('body-parser') //user bole post json
 
-app.use(bodyParser.json())
+app.use(bodyParser.json())//middleware utk handle request json
 
-app.use('/api', router)
+app.use('/api', router)//prefix attach route
 
-app.listen(3000, function() { 
+app.listen(3000, function() { //listen port & start server
 	console.log("started")
 })
